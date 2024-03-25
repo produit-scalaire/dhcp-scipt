4b23553dda934b7299e1ee196f73e0467b18d724
Théo Deschler <theo.deschler@student-cs.fr>
2024-03-25 17:40:47 +0000

    Mise à jour de 'main.py'

--- main.py
+++ main.py
@@ -35,16 +35,10 @@ def regen_dhcp(api_client):
     build_hmi(host_mac_ip_ext, api_res, 'extension')
     build_hmi(host_mac_ip_type, api_res, 'ip_type')
 
-    template = ("host {hostname}{extension} {{\n"
-                "    hardware ethernet {mac_address};\n"
-                "    fixed-address {ipv4};\n"
-                "}}")
-
-    generate_file(template, host_mac_ip_ext, "dhcp")
-    generate_file(template, host_mac_ip_type, "dhcp.ip_type.")
+    update_bdd(host_mac_ip_ext)
+    update_bdd(host_mac_ip_type)
 
 def build_hmi(host_mac_ip, api_res, key):
-    cursor.execute("DELETE FROM hosts")
     for hmi in api_res:
         if hmi[key] not in host_mac_ip.keys():
             host_mac_ip[hmi[key]] = []
@@ -53,24 +47,19 @@ def build_hmi(host_mac_ip, api_res, key):
                                       hmi['extension'],
                                       hmi['mac_address'],
                                       hmi['ipv4']))
-            cursor.execute("INSERT INTO hosts (hmi['mac_address'], 'hw-hardware', hmi['ipv4'], hmi['hostname'] + hmi['extension']) VALUES (DECODE(REPLACE(%s, ':', ''), 'hex'), (SELECT type FROM host_identifier_type WHERE name=%s), (SELECT (%s::inet - '0.0.0.0'::inet)), %s)", (hmi['mac_address'], 'hw-address', hmi['ipv4'], hmi['hostname']))
+        
+
+def update_bdd(host_mac_ip):
+    cursor.execute("DELETE FROM hosts")
+    for key, hmi_list in host_mac_ip.items():
+        for hostname, extension, mac_address, ipv4 in hmi_list:
+            cursor.execute("INSERT INTO hosts (dhcp_identifier, dhcp_identifier_type, ipv4_address, hostname) \
+            VALUES (DECODE(REPLACE(%s, ':', ''), 'hex'), \ 
+            (SELECT type FROM host_identifier_type WHERE name=%s), \ 
+            (SELECT (%s::inet - '0.0.0.0'::inet)), \ 
+            %s %s)", \ 
+            (mac_address, 'hw-address', ipv4, hostname, extension))
     conn.commit()
-
-
-def generate_file(template, host_mac_ip, file_prefix):
-     for key, hmi_list in host_mac_ip.items():
-        dhcp_leases_content = '\n\n'.join(template.format(
-                hostname=hostname,
-                extension=extension,
-                mac_address=mac_address,
-                ipv4=ipv4
-            ) for hostname, extension, mac_address, ipv4 in hmi_list)
-
-        filename = path+'/generated/{file_prefix}{key}.list'.format(
-                key=key,
-                file_prefix=file_prefix).lower().replace(" ", "_")
-        with open(filename, 'w+') as f:
-            f.write(dhcp_leases_content)
 
 
 def reload_server():
@@ -87,20 +76,6 @@ def reload_server():
         print(err)
 
 
-def check_syntax():
-    """Vérifie la configuration du serveur DHCP."""
-    try:
-        subprocess.check_output(
-            ['/usr/kea/dhcpd', '-t', '/etc/kea/kea-dhcp4.conf'],
-            stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as err:
-        print(
-            "Erreur lors de la vérification de la configuration DHCP.\n"
-            "Code de retour: %s, Sortie:\n%s",
-            err.returncode, err.output.decode())
-        return False
-    return True
-
 api_client = Re2oAPIClient(api_hostname, api_username, api_password, use_tls=True)
 
 client_hostname = socket.gethostname().split('.', 1)[0]
@@ -114,6 +89,5 @@ for service in api_client.list("services/regen/"):
             service['service_name'] == 'dhcp' and \
             service['need_regen']:
             regen_dhcp(api_client)
-            if check_syntax():
-                api_client.patch(service['api_url'], data={'need_regen': False})
+            api_client.patch(service['api_url'], data={'need_regen': False})
 
